3bb5f88f115e5ced17a718716d607acf14ee5543
Bytewaiser <bytewaiser@gmail.com>
2023-03-26 20:11:01 +0300

    Basic Circuits starter

---
+++ basic-circuits/and_gate_tb.v
@@ -0,0 +1,27 @@
+module and_gate_tb ();
+  reg a, b;
+  wire out;
+
+  and_gate G1 (
+      a,
+      b,
+      out,
+  );
+
+  initial begin
+    $dumpfile("and_gate.vcd");
+    $dumpvars(0, and_gate_tb);
+    a = 1'b0;
+    b = 1'b0;
+    #10;
+    a = 1'b0;
+    b = 1'b1;
+    #10;
+    a = 1'b1;
+    b = 1'b0;
+    #10;
+    a = 1'b1;
+    b = 1'b1;
+    #10;
+  end
+endmodule
